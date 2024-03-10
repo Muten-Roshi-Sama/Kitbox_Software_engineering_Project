93d9af501033f66e6e6a7bec556e79cb397adeed
LeomorECAM <morarleonardo@hotmail.com>
2024-03-10 17:11:01 +0100

    mise a niveau

--- MauiAp 2/MauiApp1/MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ MauiAp 2/MauiApp1/MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9037d5b45019ab9ea6ff5fc436c1eb4bb714dd29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+de86851652072388e087ee501dac07629076b9ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
