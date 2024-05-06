cabde2c7d7ea0107b99c960c5e425f71eef18d5a
AngeEcam <195105@ecam.be>
2024-05-07 00:41:29 +0200

    add global exceptions

--- MauiAp 2/MauiApp1/MauiApp1/App.xaml.cs
+++ MauiAp 2/MauiApp1/MauiApp1/App.xaml.cs
@@ -6,8 +6,24 @@ public partial class App : Application
     {
         InitializeComponent();
 
+        
         MainPage = new AppShell();
         
+        
         Application.Current.UserAppTheme = AppTheme.Light;
+
+        // gestionnaire d'exceptions global , quand une exception pas captée par un bloc try-catch
+        AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+        {
+            Console.WriteLine("Une exception non gérée a été détectée.");
+            // se contente d'ajouter un msg d'erreur mais modifier pour avoir des logs + informer
+        };
+
+        // Pour tâches asynchrones qui n'ont pas été "observées" ou traitées
+        TaskScheduler.UnobservedTaskException += (sender, e) =>
+        {
+            Console.WriteLine("Exception dans une tâche non observée.");
+            e.SetObserved(); // Evite l'app de crash , il faut aussi faire des logs pour le suivi des errors 
+        };
     }
 }
